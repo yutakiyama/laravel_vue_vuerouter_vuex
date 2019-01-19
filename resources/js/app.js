add98768441eb8b6e04c0b08f4755dc3ae493c78
Yuta Kiyama <albero.montagna@gmail.com>
2019-01-19 17:17:40 +0900

    ストアの作成とも見込み
    ・ステート・ゲッター・ミューテーション・アクションを定義してストアオブジェクトとしてエクスポート
    ・インポートしたauth.jsをモージュールとして登録

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
+import store from './store'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store,
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 })
