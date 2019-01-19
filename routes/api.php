2a8233c62b93d1a390c24401c36b03f955dbae76
Yuta Kiyama <albero.montagna@gmail.com>
2019-01-19 16:13:36 +0900

    ログイン API 実装
    ・テストコード
    ・ルート定義
    ・コントローラー

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+// 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
