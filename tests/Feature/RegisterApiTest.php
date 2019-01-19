84832fe740f6fa9aba9e16317b74f1fc70cacad9
Yuta Kiyama <albero.montagna@gmail.com>
2019-01-19 15:53:26 +0900

    会員登録 API テストコード書く

--- tests/Feature/RegisterApiTest.php
+++ tests/Feature/RegisterApiTest.php
@@ -2,19 +2,34 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterApiTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function should_新しいユーザーを作成して返却する()
     {
-        $this->assertTrue(true);
+        $data = [
+            'name' => 'vuesplash user',
+            'email' => 'dummy@email.com',
+            'password' => 'test1234',
+            'password_confirmation' => 'test1234',
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+
+        $user = User::first();
+        $this->assertEquals($data['name'], $user->name);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['name' => $user->name]);
     }
 }
