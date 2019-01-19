3bfb2649908831317d9bb3e40f642a9908026c66
Yuta Kiyama <albero.montagna@gmail.com>
2019-01-19 16:48:44 +0900

    会員登録フォーム実装

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -23,6 +23,21 @@
         </div>
       </form>
     </div>
+    <div class="panel" v-show="tab === 2">
+      <form class="form" @submit.prevent="register">
+        <label for="username">Name</label>
+        <input type="text" class="form__item" id="username" v-model="registerForm.name">
+        <label for="email">Email</label>
+        <input type="text" class="form__item" id="email" v-model="registerForm.email">
+        <label for="password">Password</label>
+        <input type="password" class="form__item" id="password" v-model="registerForm.password">
+        <label for="password-confirmation">Password (confirm)</label>
+        <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">register</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -35,11 +50,20 @@
           email: '',
           password: ''
         },
+        registerForm: {
+          name: '',
+          email: '',
+          password: '',
+          password_confirmation: ''
+        }
       }
     },
     methods: {
       login () {
         console.log(this.loginForm)
+      },
+      register () {
+        console.log(this.registerForm)
       }
     }
   }
