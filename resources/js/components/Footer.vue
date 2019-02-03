32df67c3ebf489f48b89d7cbcd439554f884c06c
YutaKiyama <albero.montagna@gmail.com>
2019-02-03 15:01:45 +0900

    ステートの値による要素の出し分け フッター

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,8 +1,19 @@
 <template>
   <footer class="footer">
-    <button class="button button--link">Logout</button>
-    <RouterLink class="button button--link" to="/login">
+    <button v-if="isLogin" class="button button--link">Logout</button>
+    <RouterLink v-else class="button button--link" to="/login">
       Login / Register
     </RouterLink>
   </footer>
 </template>
+
+
+<script>
+  export default {
+    computed: {
+      isLogin () {
+        return this.$store.getters['auth/check']
+      }
+    }
+  }
+</script>
