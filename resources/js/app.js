984bc662da5e955ebbb24cdb16c00f892840cc4a
YutaKiyama <albero.montagna@gmail.com>
2019-02-11 19:55:14 +0900

    ログインチェックしてからアプリを生成する
    アプリ起動時Vueインスタンス生成前にcurrentUserアクションを呼び出す
    すでにstoreはインポートしているので、Vueインスタンス生成前でもアクションをdispatchメソッドで呼び出すことはできる。
    
    currentUserアクションの非同期処理が終わってからVueインスタンスが生成させるため、そして非同期処理をawaitするためにasyncメソッドのバイブにいる必要があるため、起動処理をcreateApp関数にまとめ、最後に呼び出す記述に

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,17 @@ import store from './store'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  router, // ルーティングの定義を読み込む
-  store,
-  components: { App }, // ルートコンポーネントの使用を宣言する
-  template: '<App />' // ルートコンポーネントを描画する
-})
+const createApp = async () => {
+  await  store.dispatch('auth/currentUser')
+
+  new Vue({
+    el: '#app',
+    router, // ルーティングの定義を読み込む
+    store,
+    components: { App }, // ルートコンポーネントの使用を宣言する
+    template: '<App />' // ルートコンポーネントを描画する
+  })
+
+}
+
+createApp()
