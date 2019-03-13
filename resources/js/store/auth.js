8f737105a8b9f7acbc346bc823c3649788813cb5
YutaKiyama <albero.montagna@gmail.com>
2019-03-14 07:20:00 +0900

    authストア
    auth ストアモジュールではAPI呼び出しが成功したか失敗したかを表す
    apiStatusステートを追加
    コンポーネント側ではこのapiStatusステートを参照して後続の処理を行うかどうかを判別する

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,5 +1,8 @@
+import { OK } from '../util'
+
 const state = {
-  user: null
+  user: null,
+  apiStatus: null
 }
 
 const getters = {
@@ -19,8 +22,17 @@ const actions = {
     context.commit('setUser', response.data)
   },
   async login (context, data) {
-    const response = await axios.post('/api/login', data)
-    context.commit('setUser', response.data)
+    context.commit('setApiStatus', null)
+    const response = await axios.post('/api/login', data).catch(err => err.response || err)
+
+    if (response.status === OK) {
+      context.commit('setApiStatus', true)
+      context.commit('setUser', response.data)
+      return false
+    }
+
+    context.commit('setApiStatus', false)
+    context.commit('error/setCode', response.status, { root: true })
   },
   async logout (context) {
     const response = await axios.post('/api/logout')
