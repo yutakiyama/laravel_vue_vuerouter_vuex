6caaa85a2d135e2ab32efe66b920c24d0ea8c229
YutaKiyama <albero.montagna@gmail.com>
2019-03-17 17:08:00 +0900

    error ページコンポーネント
    ・通信失敗でapiStatusがfalseのとき、トップページへ移動処理を行わないようにする
    ・算出プロパティでauthモジュールのapiStatusステートを参照
    ・apiStatusが成功だった場合にのみトップページに移動する

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -58,13 +58,20 @@
         }
       }
     },
+    computed: {
+      apiStatus () {
+        return this.$store.state.auth.apiStatus
+      }
+    },
     methods: {
       async login () {
         // authストアのloginアクションを呼び出す
         await this.$store.dispatch('auth/login', this.loginForm)
 
-        // トップページに移動する
-        this.$router.push('/')
+        if (this.apiStatus) {
+          // トップページに移動する
+          this.$router.push('/')
+        }
       },
       async register () {
         // authストアのresigterアクションを呼び出す
@@ -73,5 +80,7 @@
         this.$router.push('/')
       }
     }
+
   }
+
 </script>
