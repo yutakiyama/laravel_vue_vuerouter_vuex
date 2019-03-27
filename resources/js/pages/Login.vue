f66a1bb6eaabdd7c4c530dd2175f3ccdd8a07fba
YutaKiyama <albero.montagna@gmail.com>
2019-03-27 11:25:23 +0900

    バリデーションエラー ページコンポネント
    算出プロパティでloginErrorMessagesを参照する

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -61,6 +61,9 @@
     computed: {
       apiStatus () {
         return this.$store.state.auth.apiStatus
+      },
+      loginErrors() {
+        return this.$store.state.auth.loginErrorMessages
       }
     },
     methods: {
