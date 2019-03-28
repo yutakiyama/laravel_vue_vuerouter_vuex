2cefdcf86a45e19be8c1449aade1ff7501086c71
YutaKiyama <albero.montagna@gmail.com>
2019-03-28 14:31:15 +0900

    ファイルプレビュー機能の実装
    ・写真を表示する
    input要素に@changeを追加
    プレビュー表示領域のoutput要素を追加
    
    ・dataにプレビューのデータURLを格納するpreviewを追加
    ・methodsにonFileChangeを追加

--- resources/js/components/PhotoForm.vue
+++ resources/js/components/PhotoForm.vue
@@ -2,7 +2,10 @@
   <div v-show="value" class="photo-form">
     <h2 class="title">Submit a photo</h2>
     <form class="form">
-      <input class="form__item" type="file">
+      <input class="form__item" type="file" @change="onFileChange">
+      <output class="form__output" v-if="preview">
+        <img :src="preview" v-if="preview">
+      </output>
       <div class="form__button">
         <button type="submit" class="button button--inverse">submit</button>
       </div>
@@ -17,6 +20,43 @@
         type: Boolean,
         required: true
       }
+    },
+    data () {
+      return {
+        preview: null
+      }
+    },
+    methods: {
+      onFileChange (event) {
+        // 何も選択されていなかったら処理中断
+        if (event.target.files.length === 0) {
+          return false
+        }
+
+        // ファイルが画像ではなかったら処理中断
+        if (!event.target.files[0].type.match('image.*')) {
+          return false
+        }
+
+        // FileReaderクラスのインスタンスを取得
+        const reader = new FileReader()
+
+
+        // ファイルを読み込み終わったタイミングで実行する処理
+        reader.onload = e => {
+          // previewに読み込み結果（データURL）を代入する
+          // previewに値が入ると<output>につけたv-ifがtrueと判定される
+          // また<output>内部の<img>のsrc属性はpreviewの値を参照しているので
+          // 結果として画像が表示される
+          this.preview = e.target.result
+        }
+
+        // ファイルを読み込む
+        // 読み込まれたファイルはデータURL形式で受け取れる（上記onload参照）
+        reader.readAsDataURL(event.target.files[0])
+
+      }
+
     }
   }
 </script>
