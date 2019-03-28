8b7842bee3fd2617095e0aac99e713da5b3c6353
YutaKiyama <albero.montagna@gmail.com>
2019-03-28 17:39:51 +0900

    indexメソッドは認証ミドルウェアの適用対象から外す

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -14,7 +14,7 @@ class PhotoController extends Controller
     public function __construct()
     {
         // 認証が必要
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['index']);
     }
 
     /**
