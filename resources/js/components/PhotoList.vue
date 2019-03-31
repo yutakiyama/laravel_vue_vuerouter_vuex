3ae4730c70af8209055534bd2f40c0d26524c4a3
YutaKiyama <albero.montagna@gmail.com>
2019-03-31 13:10:39 +0900

    写真の表示 PhotoListコンポーネント作成

---
+++ resources/js/components/PhotoList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="photo-list">
+    <div class="grid">
+      <Photo
+          class="grid__item"
+          v-for="photo in photos"
+          :key="photo.id"
+          :item="photo"
+      />
+    </div>
+  </div>
+</template>
