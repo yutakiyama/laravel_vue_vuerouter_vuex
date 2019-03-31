a46256280a4b907c55ed47acbf5ac620e98c557e
YutaKiyama <albero.montagna@gmail.com>
2019-03-31 13:31:15 +0900

    ルート定義

--- resources/js/router.js
+++ resources/js/router.js
@@ -18,7 +18,11 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    component: PhotoList
+    component: PhotoList,
+    props: route => {
+      const page = route.query.page
+      return { page: /^[1-9][0-9]*$/.test(page) ? page * 1 : 1 }
+    }
   },
   {
     path: '/photos/:id', // :idはURLの変化する部分
