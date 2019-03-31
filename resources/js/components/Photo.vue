d12048a2d3ec3f0b777b23a8e089ba1afa45f830
YutaKiyama <albero.montagna@gmail.com>
2019-03-31 12:55:50 +0900

    写真の表示 Photoコンポーネント作成

---
+++ resources/js/components/Photo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="photo">
+    <figure class="photo__wrapper">
+      <img
+          class="photo__image photo__image--portrait"
+          :src="item.url"
+          :alt="`Photo by ${item.owner.name}`"
+      >
+    </figure>
+    <RouterLink
+        class="photo__overlay"
+        :to="`/photos/${item.id}`"
+        :title="`View the photo by ${item.owner.name}`"
+    >
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
